ed80bf9211064dc69a2dbc01e7d538a96fbe4b7f
tgantara@gmail.com <tgantara@gmail.com>
2023-07-28 14:39:22 +0700

    add file vercel.json

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 let morgan = require('morgan');
 const app = express()
-const port = process.env.PORT || 8081
 const bodyParser = require('body-parser');
+const port =4000;
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(morgan('dev'))
+app.get('/', (req, res) => {
+  res.send('Express JS on Vercel')
+})
 
 var routes = require('./routes');
 
