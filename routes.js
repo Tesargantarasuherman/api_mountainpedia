64c79d82c816a27e0389929704d26b284b3a65df
tgantara@gmail.com <tgantara@gmail.com>
2023-07-28 13:40:14 +0700

    add file vercel.json

--- routes.js
+++ routes.js
@@ -2,7 +2,9 @@
 
 module.exports =function(app){
     let {user}= require('./src/controllers');
-
+    app.get('/', (req, res) => {
+        res.send('Express JS on Vercel')
+    })
     app.route('/user').get(user.index)
     app.route('/user/register').post(user.register)
 }
